460ee7df9bda9c6ce3a55329faf5eb85619de967
falstack <icesilt@outlook.com>
2018-02-28 22:32:30 +0800

    add: search

--- app/Http/Controllers/CartoonRoleController.php
+++ app/Http/Controllers/CartoonRoleController.php
@@ -15,13 +15,27 @@ class CartoonRoleController extends Controller
 
     public function create(Request $request)
     {
-        return CartoonRole::insertGetId([
+        $id =  CartoonRole::insertGetId([
             'bangumi_id' => $request->get('bangumi_id'),
             'avatar' => $request->get('avatar'),
             'name' => $request->get('name'),
             'intro' => $request->get('intro'),
             'alias' => $request->get('alias')
         ]);
+
+        $now = time();
+
+        MixinSearch::create([
+            'title' => $request->get('name'),
+            'content' => $request->get('alias'),
+            'type_id' => 4,
+            'modal_id' => $id,
+            'url' => '/bangumi/' . $request->get('bangumi_id') . '/role/' . $id,
+            'created_at' => $now,
+            'updated_at' => $now
+        ]);
+
+        return $id;
     }
 
     public function list()
@@ -41,5 +55,17 @@ class CartoonRoleController extends Controller
             'intro' => $request->get('intro'),
             'alias' => $request->get('alias')
         ]);
+
+        $searchId = MixinSearch::whereRaw('type_id = ? and modal_id = ?', [4, $request->get('id')])
+            ->pluck('id')
+            ->first();
+
+        if (!is_null($searchId))
+        {
+            MixinSearch::where('id', $searchId)->update([
+                'title' => $request->get('name'),
+                'content' => $request->get('alias')
+            ]);
+        }
     }
 }
