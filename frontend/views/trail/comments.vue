bfe1d409070b43680d8a828cdbbb983bf2d47b71
falstack <icesilt@outlook.com>
2018-06-04 21:26:17 +0800

    update:

--- frontend/views/trail/comments.vue
+++ frontend/views/trail/comments.vue
@@ -1,5 +1,16 @@
+<style lang="scss">
+  #trial-comment {
+    .comment-item-img {
+      display: inline-block;
+      margin-right: 5px;
+      margin-bottom: 5px;
+      cursor: pointer;
+    }
+  }
+</style>
+
 <template>
-  <section>
+  <section id="trial-comment">
     <el-table
       :data="list"
       class="main-view"
@@ -7,7 +18,19 @@
       border>
       <el-table-column label="内容">
         <template slot-scope="scope">
-          <span>{{ scope.row.content }}</span>
+          <template v-if="checkIsHTML(scope.row.content)">
+            <div
+              v-for="(item, index) in transformerContent(scope.row.content)"
+              :key="index"
+              :class="`comment-item-${item.type}`"
+            >
+              <div v-if="item.type === 'txt'" v-html="item.data"></div>
+              <div v-else-if="item.type === 'img'" @click="openImage(item.data.key)">
+                <img width="auto" height="150" :src="$resize($CDNPrefix + item.data.key, { width: 300, mode: 2 })">
+              </div>
+            </div>
+          </template>
+          <span v-else>{{ scope.row.content }}</span>
         </template>
       </el-table-column>
       <el-table-column width="440" label="操作">
@@ -62,6 +85,24 @@
           this.$Message.error('操作失败')
           console.log(err);
         })
+      },
+      checkIsHTML (content) {
+        try {
+          JSON.parse(content)
+          return true
+        } catch (e) {
+          return false
+        }
+      },
+      transformerContent (content) {
+        try {
+          return JSON.parse(content)
+        } catch (e) {
+          return []
+        }
+      },
+      openImage (url) {
+        window.open(this.$CDNPrefix + url)
       }
     }
   }
