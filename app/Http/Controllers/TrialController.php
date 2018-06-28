260fcabc378de05f0875b938b223cc7c761a597c
falstack <icesilt@outlook.com>
2018-06-28 20:24:46 +0800

    update

--- app/Http/Controllers/TrialController.php
+++ app/Http/Controllers/TrialController.php
@@ -196,7 +196,7 @@ class TrialController extends Controller
         $comments = $comments + DB::table('video_comments')->where('state', 2)->count();
 
         return [
-            'users' => User::withTrashed()->where('state', '<>', 0)->count(),
+            'users' => User::where('state', '<>', 0)->count(),
             'posts' => Post::withTrashed()->whereIn('state', [4, 5])->count(),
             'images' => Image::withTrashed()->where('state', 2)->count(),
             'feedback' => Feedback::where('stage', 0)->count(),
